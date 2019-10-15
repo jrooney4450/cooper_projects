09f33011c70dfa57352dc7d9ad01c0ac0256726b
Justin Rooneygit config --global user.name Justin <justin@dexai.com>
2019-10-15 14:34:12 -0400

    updated hw2 for ml, tests for hw3 of modern

--- ml_bayesian/2_proj_3.7.py
+++ ml_bayesian/2_proj_3.7.py
@@ -5,103 +5,115 @@ import scipy
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 
-def plotMultivariate(mu_vector, covariance_mat):
+def plotLines(ax_number, N_target, m_N, S_N):
+    # Generate random weights with updated mean and covariance to plot 6 lines
+    p_N = 6 # number of samples
+    w0 = np.random.normal(m_N[0], S_N[0, 0], p_N)
+    w1 = np.random.normal(m_N[1], S_N[1, 1], p_N)
+
+    # Plot 6 lines given the distribution of the weights
+    for n in range(w0.size):
+        ax_number.plot(x, w0[n] + w1[n]*x, color='red')
+
+    # Plot the data points
+    for n in range(N_target):
+        ax_number.scatter(x[n], target[n], facecolors='none', edgecolors='blue')
+    
+    ax_number.set_xlabel('x')
+    ax_number.set_ylabel('y')
+
+    return
+
+def plotMultivariate(ax_number, mu_vector, covariance_mat):
     x_graphing = np.linspace(-1,1,500)
     y_graphing = np.linspace(-1,1,500)
     X, Y = np.meshgrid(x_graphing,y_graphing)
     pos = np.empty(X.shape + (2,))
     pos[:, :, 0] = X; pos[:, :, 1] = Y
-
     rv = multivariate_normal(mu_vector, covariance_mat)
-    plt.figure()
-    plt.contourf(X, Y, rv.pdf(pos))
-    # plt.scatter(w0_true, w1_true) #TODO add true point
-    plt.xlabel('w0')
-    plt.ylabel('w1')
 
-    # ax = fig.gca(projection='3d')
-    # ax.plot_surface(X, Y, rv.pdf(pos),cmap='viridis',linewidth=0)
-    # plt.set_ylabel('w1')
-    # ax.set_zlabel('Z axis')
-    # ax.set_top_view()
-    # ax.tick_params(labelrotation=45)
-    # ax.view_init(elev=90., azim=270)
+    ax_number.contourf(X, Y, rv.pdf(pos))
+    ax_number.scatter(a[0], a[1], color='white', marker='+')
+    ax_number.set_xlabel('w0')
+    ax_number.set_ylabel('w1')
 
-# # Draw lines using w0 and w1 from prior TODO add random line plotter follow covariance matrix
-# p_N = 6 # number of samples
-# w0 = np.random.normal(w, w0_sigma, p_N)
-# w1 = np.random.normal(w1_mu, w1_sigma, p_N)
+# def plotLikelihood()
 
-# fig = plt.figure()
-# ax = plt.axes()
-
-# for n in range(w0.size):
-#     ax.plot(x, w0[n] + w1[n]*x)
-
-# plt.title("data space")
-# plt.xlabel("x")
-# plt.ylabel("y")
-
-#### Attempt with first data point
-
-# Use gaussian basis functions Eq. 3.4
-# For much of the book the vector phi(x) = x
-
-def plotPosterior(alpha, beta, N_target):
-    # Matrix of features through basis functions, size Nx2
+def plotPosterior(ax_number1, ax_number2, ax_number3, N_target):
+    # Matrix of features through basis functions, size NxM, where M=2 in this case
     iota = np.array([[1, x[0]]])
     if N_target > 1:
         for i in range(N_target-1):
             add_me = np.array([[1, x[i+1]]]) # using the identity for each basis function
             iota = np.concatenate((iota, add_me), axis=0)
+    print(iota)
 
     # Inverve of updated covariance matrix
     S_N_inverse = (alpha*np.identity(2) + beta*(np.matmul(np.transpose(iota),iota))) # Eq. 3.54
-    # Updated covariance matrix of posterior 
-    S_N = np.linalg.inv(S_N_inverse) # updated variance
+    # Updated covariance matrix of posterior
+    S_N = np.linalg.inv(S_N_inverse)
 
-    # Create the target vector
+    # Create the target vector and find the new mean of the weights
     target_vector = np.array([[target[0]]])
     if N_target > 1:
         for i in range(N_target-1):
             add_me = np.array([[target[i+1]]])
             target_vector = np.concatenate((target_vector, add_me), axis=0)
-
-    m_N = beta * np.matmul(np.matmul(S_N, np.transpose(iota)), target_vector)
+    m_N = beta * np.matmul(np.matmul(S_N, np.transpose(iota)), target_vector) # Eq. 3.53
     
     # Rearrange the mean vector such that the plotter can handle
     m_N = [m_N.item(0), m_N.item(1)]
 
-    plotMultivariate(m_N, S_N)
+    # TODO determine likelihood and plot
+    weights_ml = np.matmul(np.transpose(iota), iota)
+    weights_ml = np.linalg.inv(weights_ml)
+    weights_ml = np.matmul(weights_ml, np.transpose(iota))
+    weights_ml = np.matmul(weights_ml, target_vector)
+    weights_ml = [weights_ml.item(0), weights_ml.item(1)]
+    # print(weights_ml)
+
+    # plotMultivariate(ax_number1, weights_ml, S_0)
+
+    x_like = np.linspace(-1,1,100)
+    ax_number1.plot(x_like, weights_ml[0] + weights_ml[1]*x_like)
+
+    # plotLines(ax_number3, N_target, m_N, S_N)
     return
 
 if __name__ == "__main__":
 
     # True weights we wish to recover
-    a0 = -0.3
-    a1 = 0.5
+    a = [-0.3, 0.5]
 
-    # Generate data points from weights
+    # Generate true data points from weights
     N = 20
     x = 2 * np.random.random_sample(N,) - 1
 
-    # Add noise to the data
+    # Add gaussian noise to the true data points
     noise_sigma = 0.2
     noise_mean = 0
     noise_t = np.random.normal(noise_mean, noise_sigma, x.shape)
-    target = a0 + a1*x # find true value of target
+    target = a[0] + a[1]*x # find true value of target
     target += noise_t # Eq. 3.7 add gaussian noise to target
 
-    # Plot the prior
+    # Plot the prior with values give by Bishop
     beta = (1/noise_sigma)**2
     alpha = 2.0
     m_0 = [0, 0]
     S_0 = alpha**(-1)*np.identity(2)
 
-    plotMultivariate(m_0, S_0) # plot prior
+    # plot 4x3 subplots with the same x-axis and y-axis scaling
+    fig, ((ax1, ax2, ax3), (ax4, ax5, ax6), (ax7, ax8, ax9), (ax10, ax11, ax12)) = plt.subplots(4, 3, sharex=True, sharey=True)
+    ax1.set_xlim(left = -1, right = 1)
+    ax1.set_ylim(bottom = -1, top = 1)
+
+    # # Plot the prior and six lines from the estimated weights
+    # plotMultivariate(ax2, m_0, S_0)
+    # plotLines(ax3, 0, m_0, S_0)
     
-    plotPosterior(alpha, beta, 1)
-    plotPosterior(alpha, beta, 2)
-    plotPosterior(alpha, beta, 20)
+    # Improve the model by using draws from the data
+    # plotPosterior(ax4, ax5, ax6, 1) # 1 data point
+    plotPosterior(ax7, ax8, ax9, 2) # 2 data points
+    plotPosterior(ax10, ax11, ax12, 20) # 20 data points
 
     plt.show()
--- ml_bayesian/2_proj_3.8.py
+++ ml_bayesian/2_proj_3.8.py
@@ -5,16 +5,70 @@ import scipy
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt    
 
+def basFunGauss(input_x, j):
+    # Using a gaussian basis function
+    # scale_all = 1
+    # mu = [scale_all, scale_all, scale_all, scale_all, scale_all, scale_all, scale_all, scale_all, scale_all]
+    mu = [1, 1, 1, 1, 1, 1, 1, 1, 1]
+    s = 1
+    phi_of_x = np.exp((-(input_x-mu[j])**2/(2*s**2)))
+    return phi_of_x
+
+# def genIota(M, N):
+#     iota_row = np.array([1])
+#     iota = np.zeros(1, M)
+#     # fill the first row
+#     for row in range(N):
+#         for column in range(M):
+#             print(column)
+#             add_me = np.array([basFunGauss(x[row], column)])
+#             iota = np.concatenate((iota_row, add_me), axis = 0)
+#         iota = np.concatenate((iota, , axis=0))
+#     print(iota)
+#     return
+
+def plotModel(ax_number, N):
+    # Plot original sine curve
+    ax_number.plot(x_sin, y_sin, color='green')
+
+    # Matrix of features through basis functions, size NxM, where M=9 in this case for 9 weights
+    iota = np.array([[1, basFunGauss(x[0], 0), basFunGauss(x[0], 1), basFunGauss(x[0], 2), basFunGauss(x[0], 3), basFunGauss(x[0], 4), basFunGauss(x[0], 5), basFunGauss(x[0], 6), basFunGauss(x[0], 7), basFunGauss(x[0], 8)]])
+    if N > 1:
+        for i in range(1, N):
+            add_me = np.array([[1, basFunGauss(x[i], 0), basFunGauss(x[i], 1), basFunGauss(x[i], 2), basFunGauss(x[i], 3), basFunGauss(x[i], 4), \
+                basFunGauss(x[i], 5), basFunGauss(x[i], 6), basFunGauss(x[i], 7), basFunGauss(x[i], 8)]])
+            iota = np.concatenate((iota, add_me), axis=0)
+
+    # Inverse of updated covariance matrix
+    S_N_inverse = (alpha*np.identity(10) + beta*(np.matmul(np.transpose(iota),iota))) # Eq. 3.54
+    # Updated covariance matrix of posterior
+    S_N = np.linalg.inv(S_N_inverse)
+
+    # Create the target vector and find the new mean of the weights
+    target_vector = np.array([[target[0]]])
+    if N > 1:
+        for i in range(N-1):
+            add_me = np.array([[target[i+1]]])
+            target_vector = np.concatenate((target_vector, add_me), axis=0)
+    m_N = beta * np.matmul(np.matmul(S_N, np.transpose(iota)), target_vector) # Eq. 3.53
+
+    # Plot the regression line with the calculated weights
+    x_plot = np.linspace(-0.1, 1.1, 100)
+    ax_number.plot(x_plot, m_N[0] + m_N[1]*x_plot + m_N[2]*x_plot**2 + m_N[3]*x_plot**3 + m_N[4]*x_plot**4 + m_N[5]*x_plot**5 + m_N[6]*x_plot**6 + m_N[7]*x_plot**7 + m_N[8]*x_plot**8 + m_N[9]*x_plot**9, color = 'red')
+
+    print(m_N)
+
+    # Plot the data points
+    for n in range(N):
+        ax_number.scatter(x[n], target[n], facecolors='none', edgecolors='blue')
+
 if __name__ == "__main__":
 
-    # Plot the original sine curve
-    fig = plt.figure()
-    ax = plt.axes()
+    # Generate points for the original sine curve
     x_sin = np.linspace(0, 1, 500)
     y_sin = np.sin(math.pi*2*x_sin)
-    ax.plot(x_sin, y_sin, color='green')
 
-    # Need data sets of N = 1, 2, 4, 25
+    # Need random data set of max N = 25
     N = 25
     x = np.random.random_sample(N,)
     noise_sigma = 0.1
@@ -22,29 +76,22 @@ if __name__ == "__main__":
     noise_t = np.random.normal(noise_mean, noise_sigma, x.shape)
     target = np.sin(math.pi*2*x) # find true value of target
     target += noise_t # Eq. 3.7 add gaussian noise to target
-    print(target)
-    ax.scatter(x, target, s=50, facecolors='none', edgecolor='blue', )
 
+    beta = (1/noise_sigma)**2
+    alpha = 2.0
+    m_0 = [0, 0]
+    S_0 = alpha**(-1)*np.identity(2)
 
-    # subplotting code
-    # # plot 2x2 subplots with the same x-axis and y-axis
-    # fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, sharex=True, sharey=True)
-    # beta.stats(a, b, moments='mvsk')
+    # plot 2x2 subplots with the same x-axis and y-axis
+    fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, sharex=True, sharey=True)
+    ax1.set_xlim(left = -0.1, right = 1.1)
+    ax1.set_ylim(bottom = -1.2, top = 1.2)
 
-    # # first subplot is the beta distribution of the initial hyperparameters
-    # ax1.plot(x, beta.pdf(x, a, b))
-    # ax1.set_title(title + 'Initial Guess')
+    # print(genIota(9, 25))
 
-    # # second subplot has 1/3 of the data accounted for
-    # ax2.plot(x, beta.pdf(x, a + int(m*(1/3)), b + int(l*(1/3))))
-    # ax2.set_title('Through 1/3 Data')
-    
-    # # third subplot has 2/3 of the data accounted for
-    # ax3.plot(x, beta.pdf(x, a + int(m*(2/3)), b + int(l*(2/3))))
-    # ax3.set_title('Through 2/3 Data')
-    
-    # # fourth subplot has all of the data
-    # ax4.plot(x, beta.pdf(x, a+m, b+l))
-    # ax4.set_title("Through all Data")
+    plotModel(ax1, 1)
+    plotModel(ax2, 2)
+    plotModel(ax3, 4)
+    plotModel(ax4, 25)
 
     plt.show()
