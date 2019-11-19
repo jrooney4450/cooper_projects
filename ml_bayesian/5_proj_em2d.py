c5a5c1409117e463d8641c44bfceb7ddd1f766f1
Justin Rooneygit config --global user.name Justin <justin@dexai.com>
2019-11-19 15:03:43 -0500

    2D runs stably - need to add plots

--- ml_bayesian/5_proj_em2d.py
+++ ml_bayesian/5_proj_em2d.py
@@ -26,28 +26,8 @@ if __name__ == "__main__":
     data2 = np.random.multivariate_normal(mu2, cov2, size=x.shape)
     data3 = np.random.multivariate_normal(mu3, cov3, size=x.shape)
 
-    # print(mu1.reshape(-1,1))
-
-    # plot some histograms
-    plt.figure(1)
-    # plt.xlim(-1, 1)
-    # plt.ylim(-1, 1)
-    plt.xlabel('x')
-    plt.ylabel('y')
-    plt.title('Expectation Maximization 2D')
-
-    plt.scatter(data1[:,0], data1[:,1], color = 'r')
-    plt.scatter(data2[:,0], data2[:,1], color = 'b')
-    plt.scatter(data3[:,0], data3[:,1], color = 'g')
-
-    # # Plot as seperate datasets
-    # no_bins = 150
-
-
     # # Plot as single data draw, infer seperate gaussians
-    data = np.vstack((data1, data2, data3))
-
-    # # count, bins, ignored = plt.hist(data, no_bins, density=True) 
+    data = np.vstack((data1, data2, data3)) 
 
     K = 3
     M = data.shape[1]
@@ -74,19 +54,17 @@ if __name__ == "__main__":
     cov_old = np.vstack((cov_old, cov_old, cov_old))
     pis_new = [0, 0, 0]
 
-    # print(data[0,:])
-    # print(mu1)
-    # print(cov1)
-    # prob = multivariate_normal.pdf(np.array([-0.7, -0.5]), mu1, beta*cov1)
-    # print(prob)
-    # # print(pis_old[0])
-
     j = 1
     sol = pis_old[j] * multivariate_normal.pdf([0, 0], mus_old[j, :], cov_old[j*2:j*2+M,:])
     print(sol)
 
+    # Initialize loop variables
+    thresh = 0.01
     counter = 0
-    for i in range(10): # TODO make a while loop to check proper convergence criterion
+    ll_prev = 0
+    ll_diff = 10
+    # for i in range(10): # TODO make a while loop to check proper convergence criterion
+    while ll_diff > thresh: # Checks that the difference in the log-likelihood approaches zero
         # print('start of outer loop i = {}'.format(i))
         for k in range(K):
             # print('start of k loop k = {}'.format(k))
@@ -123,33 +101,56 @@ if __name__ == "__main__":
             # Eq. 9.28 - covariance formulation
             cov_new = (1 / N_k_count) * sig_count
             print(cov_new.shape)
-            # if k == 0:
-            #     cov_new = (1 / N_k_count) * sig_count
-            # else:
-            #     cov_stack = (1 / N_k_count) * sig_count
-            #     cov_new = np.vstack((cov_new, cov_stack))
-            # print(cov_new)
-            # cov_new[k*2:k*2+M,M] = (1 / N_k_count) * sig_count # Eq. 9.28
 
-            # Add some smoothing to the covariance 
-            cov_low = 0.1
-            cov_high = 1.0
-            if cov_new[0,0] < cov_low and cov_new[1,1] < cov_low:
-                cov_new += np.array([[0.1, 0], [0, 0.1]])
-            elif cov_new[0,0] > cov_high and cov_new[1,1] > cov_high:
-                cov_new -= np.array([[0.1, 0], [0, 0.1]])
+            if k == 0:
+                cov_new = (1 / N_k_count) * sig_count
+            else:
+                cov_stack = (1 / N_k_count) * sig_count
+                cov_new = np.vstack((cov_new, cov_stack))
+            print(cov_new)
+
+            # # Add some smoothing to the covariance 
+            # cov_low = 0.1
+            # cov_high = 1.0
+            # if cov_new[0,0] < cov_low and cov_new[1,1] < cov_low:
+            #     cov_new += np.array([[0.1, 0], [0, 0.1]])
+            # elif cov_new[0,0] > cov_high and cov_new[1,1] > cov_high:
+            #     cov_new -= np.array([[0.1, 0], [0, 0.1]])
 
             if k > 0:
                 cov_stack = (1 / N_k_count) * sig_count
                 cov_new = np.vstack((cov_new, cov_stack))
 
+        # Evaluate the log likelihood - Eq. 9.28
+        # Use to determine when EM cannot find a better solution
+        ll = 0
+        for n in range(N):
+            ll_count = 0
+            x = data[n, :]
+            for k in range(K):
+                ll_count += pis_old[k] * multivariate_normal.pdf(x, mus_old[k, :], cov_old[k*2:k*2+M,:])
+            ll += np.log(ll_count)
+        ll_diff = np.abs(ll - ll_prev)
+        ll_prev = ll
+        print('log likelihood {}\nll difference {}'.format(ll, ll_diff))
+
         mus_old = mus_new; cov_old = cov_new; pis_old = pis_new
         counter += 1
 
-        print('k = \n{}\n, Mus \n{}\nSigs \n{}\nPis \n{}\n'.format(k, mus_old, cov_old, pis_old))
-        # print("Mus {}, Sigs {}, Pis {}".format(mus, sigs, pis))
+        # print('k = \n{}\n, Mus \n{}\nSigs \n{}\nPis \n{}\n'.format(k, mus_old, cov_old, pis_old))
+        # # print("Mus {}, Sigs {}, Pis {}".format(mus, sigs, pis))
 
     print('Final values!\nActual Means: \n{}\nEM Means: \n{}\nActual Sigmas: \n{}\nEM Sigmas: \n{}\
         \nafter {} iterations'.format(mus_true, mus_new, cov_true, cov_new, counter))
 
-    # plt.show()
+    # plot original data as a scatter plot
+    plt.figure(1)
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.title('Expectation Maximization 2D')
+
+    plt.scatter(data1[:,0], data1[:,1], color = 'r')
+    plt.scatter(data2[:,0], data2[:,1], color = 'b')
+    plt.scatter(data3[:,0], data3[:,1], color = 'g')
+
+    plt.show()
