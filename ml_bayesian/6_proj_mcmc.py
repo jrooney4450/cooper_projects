a0ad6888d0b4a47e06dee8f21989b53c36ff55d7
Justin Rooneygit config --global user.name Justin <justin@dexai.com>
2019-12-11 08:57:07 -0500

    feat: MCMC progress
    MCMC is sometimes stable. Created functions for creation of the design vector and iota for bayesian regression that effectively parameterize M. Cleaned up code for rejection sampling for final submission

--- ml_bayesian/6_proj_mcmc.py
+++ ml_bayesian/6_proj_mcmc.py
@@ -12,69 +12,19 @@ def basFunGauss(input_x, mu):
     phi_of_x = (1 / sig*2*np.pi**(1/2)) * np.exp((-((input_x-mu)**2)/(2*sig**2)))
     return phi_of_x
 
-def plotModel(ax_number, N, title):
-    # Plot original sine curve
-    ax_number.plot(x_sin, y_sin, color='green')
+def getPhi(M, x1, x2, i):
+    phi = np.zeros(M,)
+    phi[0] = 1
+    for m in range(M - 1):
+        phi[m + 1] = basFunGauss(x1[i], x2[m])
+    return phi
 
-    # Construct iota, the matrix of x values through basis functions, size N x M, where M = 9 in this case
-    iota = np.array([[1, basFunGauss(x[0], x[0]), basFunGauss(x[0], x[1]), basFunGauss(x[0], x[2]), basFunGauss(x[0], x[3]), basFunGauss(x[0], x[4]), \
-        basFunGauss(x[0], x[5]), basFunGauss(x[0], x[6]), basFunGauss(x[0], x[7]), basFunGauss(x[0], x[8])]])
-    for i in range(1, N):
-        add_me = np.array([[1, basFunGauss(x[i], x[0]), basFunGauss(x[i], x[1]), basFunGauss(x[i], x[2]), basFunGauss(x[i], x[3]), basFunGauss(x[i], x[4]), \
-            basFunGauss(x[i], x[5]), basFunGauss(x[i], x[6]), basFunGauss(x[i], x[7]), basFunGauss(x[i], x[8])]])
-        iota = np.concatenate((iota, add_me), axis=0) 
 
-    # Update the covariance matrix
-    S_N = np.linalg.inv(alpha*np.identity(10) + beta*(np.matmul(np.transpose(iota),iota))) # Eq. 3.54
-
-    # Create the y vector based on the data draw and use find the new mean of the weights
-    target_vector = np.array([[y[0]]])
-    for i in range(N-1):
-        add_me = np.array([[y[i+1]]])
-        target_vector = np.concatenate((target_vector, add_me), axis=0)
-    m_N = beta * np.matmul(np.matmul(S_N, np.transpose(iota)), target_vector) # Eq. 3.53
-    print(m_N)
-
-    # Calculate the regression line for a given domain
-    j = 100
-    x_plot = np.linspace(-0.1, 1.1, j)
-    mean_list = []
-    low_list = []
-    high_list = []
-    for i in range(j):
-        # phi of x, running through basis functions
-        design_vector = np.transpose(np.array([[1, basFunGauss(x_plot[i], x[0]), basFunGauss(x_plot[i], x[1]), basFunGauss(x_plot[i], x[2]), \
-            basFunGauss(x_plot[i], x[3]), basFunGauss(x_plot[i], x[4]), basFunGauss(x_plot[i], x[5]), \
-            basFunGauss(x_plot[i], x[6]), basFunGauss(x_plot[i], x[7]), basFunGauss(x_plot[i], x[8])]]))
-        mean_arr = np.matmul(np.transpose(m_N), design_vector)
-        # Calculate the variance following Eq. 3.59
-        variance_arr = 1/beta + np.matmul(np.matmul(np.transpose(design_vector), S_N), design_vector)
-        low_arr = mean_arr - variance_arr**(1/2)
-        high_arr = mean_arr + variance_arr**(1/2)
-        mean_list.append(mean_arr[0,0])
-        low_list.append(low_arr[0,0])
-        high_list.append(high_arr[0,0])
-
-    # Plot the regression line with high and low variance bounding lines
-    ax_number.plot(x_plot, mean_list, color='red')
-    # ax_number.plot(x_plot, mean_arr, color='red')
-    ax_number.fill_between(x_plot, low_list, high_list, color='mistyrose')
-    # ax_number.fill_between(x_plot, low_arr, high_arr, color='mistyrose')
-    ax_number.set_xlabel('x')
-    ax_number.set_ylabel('t')
-    ax_number.set_title(title)
-
-    # Plot the draw of data points
-    for n in range(N):
-        ax_number.scatter(x[n], y[n], facecolors='none', edgecolors='blue')
-
-    return
-
-def plotMCMCModel(x, t, N):
+def plotMCMCModel(x, t, N, x_sin, y_sin):
     x = x[0:N]
     t = t[0:N]
 
-    M = 4 # Number of weights
+    M = 2 # Number of weights
     w = (np.random.random_sample(M) * 2) - 1 # random guess for the weights
 
     # parameters
@@ -84,7 +34,20 @@ def plotMCMCModel(x, t, N):
 
     # construt a prior
     m0 = np.zeros((M,))
-    S0 = np.identity(M)
+    k = 1
+    S0 = k * np.identity(M)
+    
+    iota = np.zeros((N, M))
+    for i in range(N):
+        iota[i, :] = getPhi(M, x, x, i)
+
+    # # Update the covariance matrix
+    t = t.reshape(-1, 1)
+    S_N = np.linalg.inv(alpha*np.identity(M) + beta*(np.matmul(np.transpose(iota), iota))) # Eq. 3.54
+    m_N = beta * np.matmul(np.matmul(S_N, np.transpose(iota)), t) # Eq. 3.53
+
+
+##############################################################################################
 
     S = 0.04*S0 # Const cov for mean refresh
 
@@ -94,35 +57,73 @@ def plotMCMCModel(x, t, N):
     p = [0, 0] # probabilities of z_prev and z_star
     log_prior = multivariate_normal.pdf(z[0], m0, S0)
 
+    # Make a movie! Plot dataset with ellipse for newly calculated parameters
+    fig, ax_loop = plt.subplots()
+    ax_loop.set_title('2D Expectation Maximization')
+    ax_loop.set_xlabel('w0')
+    ax_loop.set_ylabel('w1') 
+    ax_loop.set_xlim(-5, 5)
+    ax_loop.set_ylim(-5, 5)
+
+    log_prior = np.log(multivariate_normal.pdf(z[0], m0, S0))
+
+    min_like = 0.0 # store point with maximum likelihood
+    min_like_z = np.zeros((2,))
+
     for _ in range(10000):
         for k in range(2):
             like_sum = 0
 
             for i in range(N):
-                phi = np.array([1, basFunGauss(x[i], x[0]), basFunGauss(x[i], x[1]), basFunGauss(x[i], x[2])])
-                like = np.abs(mlab.normpdf(t[i], np.dot(z[k], phi), beta**(-1))) # Eq. 3.10 for likelihood
+                # phi = np.array([1, basFunGauss(x[i], x[0]), basFunGauss(x[i], x[1]), basFunGauss(x[i], x[2])]) # M = 4
+                # phi = np.array([1, basFunGauss(x[i], x[0])])
+                phi = getPhi(M, x, x, i)
+                like = mlab.normpdf(t[i], np.dot(z[k], phi), beta**(-1)) # Eq. 3.10 for likelihood
                 like_sum += like
             
             log_like = np.log(like_sum)
-            log_prior = np.log(np.abs(multivariate_normal.pdf(z[k], m0, S)))
+            # print('{} log likelihood'.format(log_like))
+            log_prior = np.log(multivariate_normal.pdf(z[k], m0, S0))
             p[k] = log_prior + log_like # this is the posterior
+
+            if np.abs(log_like) < min_like:
+                min_like_z = z[k]
 
         u = np.random.uniform(0, 1)
         A = min([1.0, p[0] / p[1]]) # Eq. 11.33 - Metropolis criterion
+
+        print('{} log likelihood'.format(log_like))
+        print('{}: old weights'.format(m_N[:,0]))
+        print('{}: best weight'.format(min_like_z))
+        print('{}: new weights\n'.format(z[0]))
+
         if A > u: # accept this point
-            print('point accepted')
+            # print('point accepted')
+            ax_loop.scatter(z[0][0], z[0][1], color = 'red')
+            ax_loop.scatter(z[1][0], z[1][1], color = 'green', s=0.8) # plot z_star as success
             z[0] = z[1] # z_star assigned to z_prev
             z[1] = np.random.multivariate_normal(z[0], S) # draw another data point
             # log_prior = p[1] # set equal to previous posterior
+
         else: # reject this point 
-            print('point rejected')
+            # print('point rejected')
+            ax_loop.scatter(z[1][0], z[1][1], color = 'red', s=0.8) # plot z_star as failure
             z[1] = np.random.multivariate_normal(z[0], S) # try again with another data point
 
+        # # Make a movie
+        # plt.ion()
+        # plt.show(block=False)
+        # plt.pause(0.1)
+        # plt.close()
 
-    m_N = z[0].reshape(-1, 1)
-    print('weights from mcmc: {}'.format(m_N))
+    ax_loop.scatter(z[0][0], z[0][1], color = 'cyan', marker='x', s=2)
+    ax_loop.scatter(min_like_z[0], min_like_z[1], color = 'black', marker='x', s=2)
 
-    S_N = S
+    # Chose which value to use from MCMC
+    print('last weight from mcmc: {}'.format(z[0]))
+    print('min like weight from mcmc: {}'.format(min_like_z))
+    # best_z = z[0].reshape(-1, 1)
+    best_z = min_like_z.reshape(-1, 1)
 
     # Calculate the regression line for a given domain
     I = 100
@@ -132,15 +133,15 @@ def plotMCMCModel(x, t, N):
     high_list = []
     for i in range(I):
         # phi of x, running through basis functions
-        design_vector = np.transpose(np.array([1, basFunGauss(x_plot[i], x[0]), basFunGauss(x_plot[i], x[1]), basFunGauss(x_plot[i], x[2])]))
-        mean_arr = np.matmul(np.transpose(m_N), design_vector)
+        design_vector = getPhi(M, x_plot, x, i).reshape(-1, 1)
+        mean_arr = np.matmul(np.transpose(best_z), design_vector)
         # Calculate the variance following Eq. 3.59
-        variance_arr = 1/beta + np.matmul(np.matmul(np.transpose(design_vector), S_N), design_vector)
+        variance_arr = 1/beta + np.matmul(np.matmul(np.transpose(design_vector), S), design_vector)
         low_arr = mean_arr - variance_arr**(1/2)
         high_arr = mean_arr + variance_arr**(1/2)
-        mean_list.append(mean_arr[0])
-        low_list.append(low_arr[0])
-        high_list.append(high_arr[0])
+        mean_list.append(mean_arr[0, 0])
+        low_list.append(low_arr[0, 0])
+        high_list.append(high_arr[0, 0])
         # Plot the regression line with high and low variance bounding lines
     
     fig, ax2 = plt.subplots()
@@ -155,49 +156,42 @@ def plotMCMCModel(x, t, N):
     for n in range(N):
         ax2.scatter(x[n], t[n], facecolors='none', edgecolors='blue')
 
-##############################################################################################
+#############################################################################################
 
-    # # Do regression the previous way for comparison
-    # iota = np.array([[1, basFunGauss(x[0], x[0]), basFunGauss(x[0], x[1]), basFunGauss(x[0], x[2])]])
-    # for i in range(1, N):
-    #     add_me = np.array([[1, basFunGauss(x[i], x[0]), basFunGauss(x[i], x[1]), basFunGauss(x[i], x[2])]])
-    #     iota = np.concatenate((iota, add_me), axis=0) 
+    # Calculate the regression line for a given domain
+    I = 100
+    x_plot = np.linspace(-0.1, 1.1, I)
+    mean_list = []
+    low_list = []
+    high_list = []
 
-    # # # Update the covariance matrix
-    # t = t.reshape(-1, 1)
-    # S_N = np.linalg.inv(alpha*np.identity(M) + beta*(np.matmul(np.transpose(iota), iota))) # Eq. 3.54
-    # m_N = beta * np.matmul(np.matmul(S_N, np.transpose(iota)), t) # Eq. 3.53
-    # print('Old weights are: {}'.format(m_N))
+    for i in range(I):
+        design_vector = getPhi(M, x_plot, x, i).reshape(-1, 1)
+        mean_arr = np.matmul(np.transpose(m_N), design_vector)
 
-    # # Calculate the regression line for a given domain
-    # mean_list = []
-    # low_list = []
-    # high_list = []
-    # for i in range(I):
-    #     # phi of x, running through basis functions
-    #     design_vector = np.transpose(np.array([1, basFunGauss(x_plot[i], x[0]), basFunGauss(x_plot[i], x[1]), basFunGauss(x_plot[i], x[2])]))
-    #     mean_arr = np.matmul(np.transpose(m_N), design_vector)
-    #     # Calculate the variance following Eq. 3.59
-    #     variance_arr = 1/beta + np.matmul(np.matmul(np.transpose(design_vector), S_N), design_vector)
-    #     low_arr = mean_arr - variance_arr**(1/2)
-    #     high_arr = mean_arr + variance_arr**(1/2)
-    #     mean_list.append(mean_arr[0])
-    #     low_list.append(low_arr[0])
-    #     high_list.append(high_arr[0])
-    #     # Plot the regression line with high and low variance bounding lines
+        # Calculate the variance following Eq. 3.59
+        variance_arr = 1/beta + np.matmul(np.matmul(np.transpose(design_vector), S_N), design_vector)
+        low_arr = mean_arr - variance_arr**(1/2)
+        high_arr = mean_arr + variance_arr**(1/2)
+        mean_list.append(mean_arr[0,0])
+        low_list.append(low_arr[0,0])
+        high_list.append(high_arr[0,0])
+        
+    # Plot the regression line with high and low variance bounding lines
+    fig, ax_number = plt.subplots()
+    ax_number.plot(x_plot, mean_list, color='red')
+    ax_number.fill_between(x_plot, low_list, high_list, color='mistyrose')
+
+    ax_number.set_xlabel('x')
+    ax_number.set_ylabel('t')
+    ax_number.set_title('bayesian linear regression')
     
-    # fig, ax_number = plt.subplots()
-    # ax_number.plot(x_plot, mean_list, color='red')
-    # ax_number.fill_between(x_plot, low_list, high_list, color='mistyrose')
+    # Plot the draw of data points
+    for n in range(N):
+        ax_number.scatter(x[n], t[n], facecolors='none', edgecolors='blue')
 
-    # ax_number.set_xlabel('x')
-    # ax_number.set_ylabel('t')
-    # ax_number.set_title('bayesian linear regression')
-    
-    # # Plot the draw of data points
-    # for n in range(N):
-    #     ax_number.scatter(x[n], t[n], facecolors='none', edgecolors='blue')
-            
+    # Plot the curve
+    ax_number.plot(x_sin, y_sin, color='green')
 
 def main():
     # Generate data points for the original sine curve
@@ -234,9 +228,9 @@ def main():
 
     # plt.figure(2)
     N = 25
-    plotMCMCModel(x, t, N)
+    plotMCMCModel(x, t, N, x_sin, y_sin)
 
-    plt.show()
+    plt.show(block=True)
 
 if __name__ == "__main__":
     main()
--- ml_bayesian/6_proj_rejection_sampling.py
+++ ml_bayesian/6_proj_rejection_sampling.py
@@ -7,9 +7,8 @@ from matplotlib.patches import Ellipse
 import matplotlib.transforms as transforms
 
 def main():
-    ########################### 1D ##################################
-    
     # Generate three gaussians to form GMM
+    K = 3
     mu1 = -1.9; mu2 = 0.2; mu3 = 2.1
     mus_true = [mu1, mu2, mu3]
     sig1 = 0.8; sig2 = 0.7; sig3 = 0.8
@@ -23,20 +22,12 @@ def main():
     colors = ['tomato', 'slateblue', 'limegreen']
     labels = ['Gaussian 1', 'Gaussian 2', 'Gaussian 3']
 
-    # Initialize loop variables
-    K = 3
-
     # Plot GMM distributions with histograms
-    plt.figure(1)
-    plt.xlabel('x')
-    plt.ylabel('Probability')
-    plt.title('Expectation Maximization PDF over Histogram')
     for k in range(K):  
-        # plt.hist(data_list[k], bin_list[k], density=True, color = colors[k])
         x = np.linspace(mus_true[k] - 3*sigs_true[k], mus_true[k] + 3*sigs_true[k], 100)
         plt.plot(x, norm.pdf(x, mus_true[k], sigs_true[k]), c=colors[k], label=labels[k])
 
-    # Big boi gaussian that covers all of GMM - k * q(z)
+    # Big boi gaussian that covers all of GMM: k * q(z)
     mu = 0.0; sig = 2.0; k = 6 
     x = np.linspace(mu - 3*sig, mu + 3*sig, 100)
     plt.plot(x, k * norm.pdf(x, mu, sig), label='kq(z)')
@@ -46,17 +37,19 @@ def main():
     data = np.random.normal(mu, sig, N)
     data_new = []
 
+    # Rejection sampling algorithm, section 11.1.2
     for pt in data:
-        kq_pt = k * norm.pdf(pt, mu, sig) # max probabilty of this particular draw
-        u = np.random.uniform(0, kq_pt) # from uniform distribution [0, kq(pt)]
+        kq_pt = k * norm.pdf(pt, mu, sig) # max probabilty of particular data draw
+        u = np.random.uniform(0, kq_pt) # pick u from uniform distribution
         if u > norm.pdf(pt, mu1, sig1) and u > norm.pdf(pt, mu2, sig2) and u > norm.pdf(pt, mu3, sig3):
-            # Reject this point
+            # Reject this point and plot success as red
             plt.scatter(pt, u, c='r', s=0.8)
         else:
-            # Keep this point
+            # Keep this point and plot success as green
             plt.scatter(pt, u, c='g', s=0.8)
             data_new.append(pt)
 
+    # Plot the resulting data draw as a histogram
     bins = 30
     plt.hist(data_new, bins, density=True, color='mistyrose', label='new data draw')
 
